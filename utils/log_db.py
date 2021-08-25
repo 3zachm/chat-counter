081f2624f612d039da01810932f940a0f7f29c17
3zachm <3zachm2@gmail.com>
2021-08-25 16:49:37 -0700

    include username

--- utils/log_db.py
+++ utils/log_db.py
@@ -1,7 +1,7 @@
 import utils.api.db as db
 
-def insert_log(user_id, message):
-    sql = "INSERT INTO logs (id, message) VALUES (%s, %s)"
-    val = (user_id, message)
+def insert_log(user_id, username, message):
+    sql = "INSERT INTO logs (id, user, message) VALUES (%s, %s, %s)"
+    val = (user_id, username, message)
     db.execute_val(sql, val)
     db.commit()
